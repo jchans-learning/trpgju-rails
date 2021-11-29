ded6d884e0d7f34e6599bd3477894bff4e91e2f0
jchans <hanschiang86@gmail.com>
2021-11-30 01:26:19 +0800

    Add handleButtonClick to control dropdown menu

--- app/javascript/components/NavBurger.js
+++ app/javascript/components/NavBurger.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 
 export default class NavBurger extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      dropDown: false,
+    }
+
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+  }
+
+  handleButtonClick = () => {
+    this.setState(prevState => ({
+      dropDown: !prevState.dropDown
+    }));
+    console.log("Menu closed: ", this.state.dropDown);
+  }
+
   render() {
     return (
-      <div>
-        {/* <button data-collapse-toggle="mobile-menu" type="button" class="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu-2" aria-expanded="false">
+      <div className="flex flex-col justify-end">
+        <button data-collapse-toggle="mobile-menu" onClick={this.handleButtonClick} type="button" class="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu-2" aria-expanded="false">
           <span class="sr-only">開啟目錄</span>
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
           <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </button>
-        <div class="hidden md:block w-full md:w-auto" id="mobile-menu">
+        <div className={(this.state.dropDown ? " md:block w-full md:w-auto origin-top-right" : "hidden md:block w-full md:w-auto origin-top-right")} id="mobile-menu">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
             <li>
               <a href="#" class="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded" aria-current="page">月曆</a>
@@ -27,70 +44,8 @@ export default class NavBurger extends Component {
               <a href="#" class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">管理</a>
             </li>
           </ul>
-        </div> */}
-
-        <button
-          id="dropdownButton"
-          data-dropdown-toggle="dropdown"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
-          type="button"
-        >
-          Dropdown button{" "}
-          <svg
-            class="w-4 h-4 ml-2"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M19 9l-7 7-7-7"
-            ></path>
-          </svg>
-        </button>
-
-        <div
-          id="dropdown"
-          class="hidden bg-white text-base z-10 list-none divide-y divide-gray-100 rounded shadow w-44"
-        >
-          <ul class="py-1" aria-labelledby="dropdownButton">
-            <li>
-              <a
-                href="#"
-                class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-              >
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-              >
-                Settings
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-              >
-                Earnings
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
-              >
-                Sign out
-              </a>
-            </li>
-          </ul>
         </div>
+
       </div>
     );
   }
